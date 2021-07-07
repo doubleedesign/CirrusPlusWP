ba14ba21a833293f5c454a8f83701c8c952da673
Leesa Ward <leesa@doubleedesign.com.au>
2021-07-07 20:14:20 +1000

    JS cleanup

--- inc/setup/enqueue.php
+++ inc/setup/enqueue.php
@@ -3,22 +3,15 @@
  * Enqueue front-end scripts and styles
  */
 function doublee_enqueue_frontend() {
-	// Dequeue existing scripts
-	wp_dequeue_script('jquery');
-	wp_deregister_script('jquery');
-
-	// Third-party styles
-
-	// Our styles
-	wp_enqueue_style('main-style', get_template_directory_uri().'/dist/css/main.min.css');
 
 	// Third-party scripts
-	wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.4.1.min.js', '', '', true);
-	if(defined('GMAPS_KEY') && GMAPS_KEY) { wp_enqueue_script('gmaps_api', 'https://maps.googleapis.com/maps/api/js?key='.GMAPS_KEY, '', '', true); }
+	if(defined('GMAPS_KEY') && GMAPS_KEY) {
+		wp_enqueue_script('gmaps_api', 'https://maps.googleapis.com/maps/api/js?key='.GMAPS_KEY, '', '', true);
+	}
 
 	// Our scripts
-	wp_enqueue_script('vendor-script', get_template_directory_uri().'/dist/js/vendor.min.js', 'jquery','', true);
-	wp_enqueue_script('main-script', get_template_directory_uri().'/dist/js/main.min.js', array('jquery', 'bootstrap'),'', true);
+	wp_enqueue_script('vendor-scripts', get_template_directory_uri().'/js/dist/vendor.min.js', array('jquery'),'', true);
+	wp_enqueue_script('theme-script', get_template_directory_uri().'/js/dist/theme.min.js', array('jquery'),'', true);
 }
 add_action('wp_enqueue_scripts', 'doublee_enqueue_frontend');
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
-jQuery(document).ready(function($){
-    $(document.body).removeClass('no-js').addClass('js');
+document.addEventListener('DOMContentLoaded', function() {
+	"use strict";
 
-    initPolyfills();
-
-    // Utilities
-    initClassToggle();
-    initAnchorScroll();
-    //initFullheightMobile();
-
+	initPolyfills();
+	initAnchorScroll();
 });
 
+window.addEventListener('load', function() {
+});
+
+window.addEventListener('resize', function() {
+});
+
+
+/**
+ * Viewport size utility function
+ * @returns {boolean}
+ */
 function isMobile() {
     return window.matchMedia('(max-width:767px)').matches;
 }
 
+
+/**
+ * Polyfills
+ */
 function initPolyfills() {
-    // CSS object-fit for IE
-    objectFitImages();
 
     // polyfill for IE - startsWith
     if (!String.prototype.startsWith) {
@@ -39,23 +47,6 @@ function initPolyfills() {
 
 
 /**
- * Toggle class on click
- */
-function initClassToggle() {
-    $(document.body).on('click', '[data-toggle="class"][data-class]', function(event) {
-        var $trigger = $(this);
-        var $target = $($trigger.data('target') ? $trigger.data('target') : $trigger.attr('href'));
-
-        if($target.length) {
-            event.preventDefault();
-            $target.toggleClass($trigger.data('class'));
-            $trigger.toggleClass('classed');
-        }
-    });
-}
-
-
-/**
  * Smooth anchor scrolling
  */
 function initAnchorScroll() {
@@ -72,46 +63,3 @@ function initAnchorScroll() {
         }
     });
 }
-
-
-/*function initFullheightMobile() {
-    // Fix mobile 100vh change on address bar show/hide
-    var lastHeight = $(window).height();
-    var heightChangeTimeout = undefined;
-    if(isMobile()) {
-        $('.vh').css('height', lastHeight);
-    }
-    (maybe_update_landing_height = function() {
-        var winHeight = $(window).height();
-
-        if(heightChangeTimeout !== undefined) {
-            clearTimeout(heightChangeTimeout);
-        }
-
-        if(!isMobile()) {
-            $('.vh').css('height', '');
-        }
-        else if(Math.abs(winHeight - lastHeight) > 100) {
-            heightChangeTimeout = setTimeout(function() {
-                var winHeight = $(window).height();
-                $('.vh').css('height', winHeight);
-                lastHeight = winHeight;
-            }, 50);
-        }
-    })();
-    $(window).resize(maybe_update_landing_height);
-}*/
-
-/*function initCopyToClipboard() {
-  $(document.body).on('click', '[data-copy-to-clipboard]', function(event) {
-    event.preventDefault();
-
-    // Source: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
-    var el = document.createElement('textarea');
-    el.value = $(this).data('copy-to-clipboard');
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-  });
-}*/
