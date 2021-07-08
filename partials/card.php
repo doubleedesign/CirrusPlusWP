cd61e24fe43df9325ce997683538f5caa7d92ea7
Leesa Ward <leesa@doubleedesign.com.au>
2021-07-08 14:25:53 +1000

    Complete basic cards

--- partials/card.php
+++ partials/card.php
@@ -1,13 +1,13 @@
-<div class="col-xs-12 col-md-6 col-lg-4">
-	<a href="<?php the_permalink(); ?>" class="card">
-		<div class="card-container">
+<div class="card-wrapper col-xs-12 col-md-6 col-lg-4">
+	<div class="card u-flex u-flex-column h-100">
+		<a href="<?php the_permalink(); ?>" class="card-container">
 			<div class="card-image">
 				<?php the_post_thumbnail(); ?>
 			</div>
-			<div class="card-content">
+			<div class="card-content entry-content">
 				<h2><?php the_title(); ?></h2>
-				<?php the_excerpt(); ?>
+				<?php echo doublee_custom_excerpt(get_the_excerpt(), 25); ?>
 			</div>
-		</div>
-	</a>
+		</a>
+	</div>
 </div>
