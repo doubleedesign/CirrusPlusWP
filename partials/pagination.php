4e64c54a6817f9b61d7274dbca1ba1d35e9107d1
Leesa Ward <leesa@doubleedesign.com.au>
2021-07-08 17:50:18 +1000

    Use Cirrus button styles for pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -2,14 +2,20 @@
 	<div class="col-12">
 		<?php
 		global $wp_query;
-		echo paginate_links(array(
-			'current'   => max( 1, get_query_var('paged')),
+		$output = paginate_links(array(
+			'current'   => max(1, get_query_var('paged')),
 			'total'     => $wp_query->max_num_pages,
 			'prev_text' => 'Prev',
 			'next_text' => 'Next',
 			'type'      => 'list',
 			'end_size'  => 3,
 			'mid_size'  => 3
-		)); ?>
+		));
+		$output = str_replace('page-numbers', 'btn btn-dark outline', $output);
+		$output = str_replace("<ul class='btn btn-dark outline'>", '<ul>', $output);
+		$output = str_replace('outline current', 'current', $output);
+
+		echo $output;
+		?>
 	</div>
 </div>
